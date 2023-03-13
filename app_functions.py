71e6e05ed72b780937d037e69523fd44835cc74e
ricardoandre <ricardo.andreom@gmail.com>
2023-03-13 23:33:50 +0000

    initial commit

---
+++ app_functions.py
@@ -0,0 +1,318 @@
+# libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+import io
+from urllib.request import urlopen
+from PIL import Image
+from mplsoccer import PyPizza, add_image, FontManager
+import matplotlib.font_manager
+from IPython.core.display import HTML
+from pathlib import Path
+
+font_normal = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/roboto/'
+                          'Roboto%5Bwdth,wght%5D.ttf')
+font_italic = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/roboto/'
+                          'Roboto-Italic%5Bwdth,wght%5D.ttf')
+font_bold = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/robotoslab/'
+                        'RobotoSlab%5Bwght%5D.ttf')
+
+
+# default fontname
+def make_html(fontname):
+    return "<p>{font}: <span style='font-family:{font}; font-size: 24px;'>{font}</p>".format(font=fontname)
+
+
+# creating dataframe function
+def get_df(url_df, columns_remaining_list):
+    df = pd.read_html(url_df)[0]
+    df.columns = [' '.join(col).strip() for col in df.columns]
+    df = df.reset_index(drop=True)
+
+    # creating a list with new names
+    new_columns = []
+    for col in df.columns:
+        if 'level_0' in col:
+            new_col = col.split()[-1]  # takes the last name
+        else:
+            new_col = col
+        new_columns.append(new_col)
+
+    # rename columns
+    df.columns = new_columns
+    df = df.fillna(0)
+
+    if 'Playing Time 90s' in new_columns:
+        df = df.rename(columns={'Playing Time 90s': '90s'})
+    else:
+        df = df.rename(columns={'90s': '90s'})
+
+    df = df[['Rk', 'Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born', '90s'] +
+            columns_remaining_list]
+
+    df['Age'] = df['Age'].str[:2]
+    df['Position_2'] = df['Pos'].str[3:]
+    df['Position'] = df['Pos'].str[:2]
+    df['Nation'] = df['Nation'].str.split(' ').str.get(1)
+    df['League'] = df['Comp'].str.split(' ').str.get(1)
+    df['League_'] = df['Comp'].str.split(' ').str.get(2)
+    df['League'] = df['League'] + ' ' + df['League_']
+    df = df.drop(columns=['League_', 'Comp', 'Rk', 'Pos', 'Born'])
+
+    df['Position'] = df['Position'].replace({'MF': 'Midfielder', 'DF': 'Defender', 'FW': 'Forward', 'GK': 'Goalkeeper'})
+    df['Position_2'] = df['Position_2'].replace({'MF': 'Midfielder', 'DF': 'Defender',
+                                                 'FW': 'Forward', 'GK': 'Goalkeeper'})
+    df['League'] = df['League'].fillna('Bundesliga')
+
+    return df
+
+
+# function to edit the df resulted by the concatenation
+def edit_df(df_concatenated):
+    df_concatenated = df_concatenated.rename(columns={'Per 90 Minutes G-PK': 'Non-penalty goals',
+                                                      'SCA SCA': 'Shot creation actions',
+                                                      'Aerial Duels Won%': 'Aerial duels won %',
+                                                      'Performance Fld': 'Fouls won',
+                                                      'Performance Crs': 'Crosses',
+                                                      'Performance Recov': 'Ball recoveries',
+                                                      'Expected npxG': 'npxG',
+                                                      'Expected npxG/Sh': 'npxG/shot',
+                                                      'Standard SoT': 'Shots on target',
+                                                      'Total Cmp': 'Passes completed',
+                                                      'KP': 'Key passes',
+                                                      '1/3': 'Final 3rd passes',
+                                                      'Long Att': 'Long passes',
+                                                      'PrgP': 'Progressive passes',
+                                                      'Total Cmp%': 'Pass completion %',
+                                                      'Tkl+Int': 'Tackles + interceptions',
+                                                      'Blocks Sh': 'Shots blocked',
+                                                      'Clr': 'Clearances',
+                                                      'Take-Ons Att': 'Dribbles attempted',
+                                                      'Take-Ons Succ%': 'Dribbles success %',
+                                                      'Carries CPA': 'Carries into penalty area',
+                                                      'Receiving Rec': 'Passes received',
+                                                      'Receiving PrgR': 'Progressive passes received',
+                                                      'Carries PrgC': 'Progressive carries'
+                                                      })
+
+    numeric_columns = ['Age', '90s', 'Non-penalty goals', 'Shot creation actions', 'Aerial duels won %', 'Fouls won',
+                       'Crosses', 'Ball recoveries', 'npxG', 'npxG/shot', 'Shots on target', 'Passes completed',
+                       'Key passes', 'xA', 'Final 3rd passes', 'Long passes', 'Progressive passes', 'Pass completion %',
+                       'Tackles + interceptions', 'Shots blocked', 'Clearances', 'Dribbles attempted',
+                       'Dribbles success %', 'Carries into penalty area', 'Passes received',
+                       'Progressive passes received', 'Progressive carries']
+
+    for j in numeric_columns:
+        df_concatenated[j] = pd.to_numeric(df_concatenated[j], errors='coerce')
+
+    ninety_columns = ['Non-penalty goals', 'Shot creation actions', 'Fouls won',
+                      'Crosses', 'Ball recoveries', 'npxG', 'Shots on target',
+                      'Passes completed', 'Key passes', 'xA', 'Final 3rd passes',
+                      'Long passes', 'Progressive passes', 'Tackles + interceptions',
+                      'Shots blocked', 'Clearances', 'Dribbles attempted',
+                      'Carries into penalty area', 'Passes received',
+                      'Progressive passes received', 'Progressive carries']
+    # age filter
+    df_concatenated = df_concatenated[df_concatenated['90s'] > 0]
+
+    for i in ninety_columns:
+        df_concatenated[i] = (df_concatenated[i]/df_concatenated['90s']).round(2)
+
+    return df_concatenated
+
+
+# calculating and adding percentile columns to df function
+def percentiles_df(df_filtered_player_position):
+    # percentile columns list
+    df_cols_pct = ['Non-penalty goals', 'Shot creation actions', 'Aerial duels won %', 'Fouls won', 'Crosses',
+                   'Ball recoveries', 'npxG', 'npxG/shot', 'Shots on target', 'Passes completed', 'Key passes', 'xA',
+                   'Final 3rd passes', 'Long passes', 'Progressive passes', 'Pass completion %',
+                   'Tackles + interceptions', 'Shots blocked', 'Clearances', 'Dribbles attempted', 'Dribbles success %',
+                   'Carries into penalty area', 'Passes received', 'Progressive passes received', 'Progressive carries']
+    for col in df_cols_pct:
+        # Calculate the percentiles for each 'param' column
+        percentiles = np.percentile(df_filtered_player_position[col], np.linspace(0, 100, num=101))
+        df_filtered_player_position[col + '_pct'] = np.searchsorted(percentiles, df_filtered_player_position[col])*(100 / (len(percentiles) - 1))
+
+    return df_filtered_player_position
+
+
+def templates_position_params_legend(player_position):
+    params = []
+    if player_position == 'Goalkeeper':
+        params = []
+    elif player_position == 'Defender':
+        params_legend = ['Non-penalty\ngoals', 'Shot creation\nactions', 'Progressive passes\nreceived',
+                         'Carries into\npenalty area', 'Progressive\ncarries',
+                         'Dribbles\nattempted', 'Dribbles\nsuccess %',
+                         'Tackles\n+\ninterceptions', 'Shots\nblocked', 'Clearances', 'Aerial duels\nwon %',
+                         'Ball\nrecoveries', 'Final 3rd\npasses', 'Long\npasses', 'Progressive\npasses', 'xA']
+
+    elif player_position == 'Midfielder':
+        params_legend = ['Non-penalty\ngoals', 'Shot creation\nactions',
+                         'Progressive\ncarries', 'Dribbles\nattempted', 'Dribbles\nsuccess %',
+                         'Tackles +\ninterceptions', 'Aerial duels\nwon %', 'Fouls\nwon',
+                         'Ball\nrecoveries', 'Passes\ncompleted', 'Pass\ncompletion %', 'Key\npasses',
+                         'Final 3rd\npasses', 'Long\npasses', 'Progressive\npasses', 'xA']
+
+    else:
+        params_legend = ['Non-penalty\ngoals', 'npxG', 'npxG/shot', 'Shots on\ntarget',
+                         'Shot creation\nactions', 'Dribbles\nattempted', 'Dribbles\nsuccess %',
+                         'Carries into\npenalty area', 'Tackles +\ninterceptions', 'Aerial duels\nwon %',
+                         'Passes\nreceived', 'Passes\ncompleted', 'Crosses', 'Key\npasses', 'xA']
+
+    return params_legend
+
+
+def templates_position_params(player_position):
+    params = []
+    if player_position == 'Goalkeeper':
+        params = []
+    elif player_position == 'Defender':
+        params = ['Non-penalty goals_pct', 'Shot creation actions_pct', 'Progressive passes received_pct',
+                  'Carries into penalty area_pct', 'Progressive carries_pct',
+                  'Dribbles attempted_pct', 'Dribbles success %_pct',
+                  'Tackles + interceptions_pct', 'Shots blocked_pct', 'Clearances_pct', 'Aerial duels won %_pct',
+                  'Ball recoveries_pct', 'Final 3rd passes_pct', 'Long passes_pct',
+                  'Progressive passes_pct',
+                  'xA_pct']
+
+    elif player_position == 'Midfielder':
+        params = ['Non-penalty goals_pct', 'Shot creation actions_pct',
+                  'Progressive carries_pct', 'Dribbles attempted_pct', 'Dribbles success %_pct',
+                  'Tackles + interceptions_pct', 'Aerial duels won %_pct', 'Fouls won_pct',
+                  'Ball recoveries_pct', 'Passes completed_pct', 'Pass completion %_pct', 'Key passes_pct',
+                  'Final 3rd passes_pct', 'Long passes_pct', 'Progressive passes_pct', 'xA_pct']
+
+    else:
+        params = ['Non-penalty goals_pct', 'npxG_pct', 'npxG/shot_pct', 'Shots on target_pct',
+                  'Shot creation actions_pct', 'Dribbles attempted_pct', 'Dribbles success %_pct',
+                  'Carries into penalty area_pct', 'Tackles + interceptions_pct', 'Aerial duels won %_pct',
+                  'Passes received_pct', 'Passes completed_pct', 'Crosses_pct', 'Key passes_pct', 'xA_pct']
+
+    return params
+
+
+def templates_position_slice_colors(player_position):
+    if player_position == 'Goalkeeper':
+        slice_colors = []
+    elif player_position == 'Defender':
+        slice_colors = ["#4DB3F7"] * 3 + ["#E25A5A"] * 4 + ["#7DE55D"] * 5 + ["#FDE050"] * 4
+    elif player_position == 'Midfielder':
+        slice_colors = ["#4DB3F7"] * 2 + ["#E25A5A"] * 3 + ["#7DE55D"] * 4 + ["#FDE050"] * 7
+    else:
+        slice_colors = ["#4DB3F7"] * 5 + ["#E25A5A"] * 3 + ["#7DE55D"] * 2 + ["#FDE050"] * 5
+
+    return slice_colors
+
+
+def templates_position_text_colors(player_position):
+    if player_position == 'Goalkeeper':
+        text_colors = []
+    elif player_position == 'Defender':
+        text_colors = ["#000000"] * 10 + ["black"] * 6
+
+    elif player_position == 'Midfielder':
+        text_colors = ["#000000"] * 10 + ["black"] * 6
+
+    else:
+        text_colors = ["#000000"] * 10 + ["black"] * 5
+
+
+# create radar chart plot
+def show_radar_chart(player_values, df, player_position, player, nineties_played, circle_logo):
+    # instantiate PyPizza class
+    baker = PyPizza(
+        params=templates_position_params_legend(player_position),                  # list of parameters
+        background_color="white",         # background color
+        straight_line_color="white",      # color for straight lines
+        straight_line_lw=2.5,             # linewidth for straight lines
+        last_circle_lw=0,                 # linewidth of last circle
+        other_circle_ls="-.",             # linewidth for other circles
+        other_circle_lw=2,
+        inner_circle_size=19.5            # size of inner circle
+    )
+
+    # plot pizza
+    fig, ax = baker.make_pizza(
+        player_values,                          # list of values
+        figsize=(12, 12),                       # adjust figsize according to your need
+        color_blank_space="same",               # use same color to fill blank space
+        slice_colors=templates_position_slice_colors(player_position),       # color for individual slices
+        value_colors=templates_position_text_colors(player_position),                   # color for the value-text
+        value_bck_colors=templates_position_slice_colors(player_position),   # color for the blank spaces
+        blank_alpha=0.2,                        # alpha for blank-space colors
+        kwargs_slices=dict(
+            edgecolor="#F2F2F2", zorder=2, linewidth=1
+        ),                                      # values to be used when plotting slices
+        kwargs_params=dict(
+            color="#000000", fontsize=18,
+            fontproperties=font_bold.prop, va="center"
+        ),                                      # values to be used when adding parameter
+        kwargs_values=dict(
+            color="black", fontsize=15,
+            fontproperties=font_bold.prop, zorder=3,
+            bbox=dict(
+                edgecolor="#000000", facecolor="cornflowerblue",
+                boxstyle="round,pad=0.2", lw=2
+            )
+        )                                      # values to be used when adding parameter-values
+    )
+
+    # add title
+    fig.text(
+        0.515, 1.04, player + " - " + df.set_index('Player').loc[player, 'Squad'], size=40,
+        ha="center", fontproperties=font_bold.prop, fontweight='heavy', color="black"
+    )
+
+    # add subtitle
+    fig.text(
+        0.515, 0.995,
+        "Percentile Rank vs Top-Five League " + player_position + "s",
+        size=22,
+        ha="center", fontproperties=font_bold.prop, color="black"
+    )
+
+    # add text
+    fig.text(0.5, 0.955, player_position + " player stats/90" + "   | 90s played:  " +
+             str(df.set_index('Player').loc[player, '90s']) +
+             "    | Age:  " + str(int(df.set_index('Player').loc[player, 'Age'])) +
+             '   | Season 2022/23', size=20, ha='center', fontproperties=font_bold.prop, color="black")
+
+    # add text
+    text1 = "Only players with >=" + str(nineties_played) + "'s played"
+    text2 = "Data from FBREF"
+
+    # add text
+    fig.text(
+        0.68, 0.02, f"{text1}\n{text2}", size=16,
+        fontproperties=font_bold.prop, color="#000000",
+        ha="left"
+    )
+
+    # add text
+    fig.text(
+        0.05, 0.02, "Made by @ricardoandreom / @HspaceAnalytics", size=12,
+        fontproperties=font_bold.prop, color="black",
+        ha="left"
+    )
+
+    # add image
+    ax_image = add_image(
+        circle_logo, fig, left=0.417, bottom=0.41, width=0.19, height=.175
+    )
+
+    # plot image
+    buf = io.BytesIO()
+    plt.savefig(buf, dpi=500, bbox_inches='tight', facecolor='white', format="png")
+    st.image(buf.getvalue())
+
+
+# Save image function
+def save_image():
+    buf1 = io.BytesIO()
+    plt.savefig(buf1, dpi=500, bbox_inches='tight', facecolor='white', format="png")
+    # Return buffer content
+    return buf1.getvalue()
+
---
+++ app_streamlit.py
@@ -0,0 +1,176 @@
+# libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+import io
+from urllib.request import urlopen
+from PIL import Image
+from mplsoccer import PyPizza, add_image, FontManager
+import matplotlib.font_manager
+from IPython.core.display import HTML
+from pathlib import Path
+import app_functions as app
+
+# data urls list
+url_standard = 'https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats#stats_standard'
+url_shotcreation = 'https://fbref.com/en/comps/Big5/gca/players/Big-5-European-Leagues-Stats#stats_gca'
+url_misc = 'https://fbref.com/en/comps/Big5/misc/players/Big-5-European-Leagues-Stats#stats_misc'
+url_shoot = 'https://fbref.com/en/comps/Big5/shooting/players/Big-5-European-Leagues-Stats#stats_shooting'
+url_passing = 'https://fbref.com/en/comps/Big5/passing/players/Big-5-European-Leagues-Stats#stats_passing'
+url_def = 'https://fbref.com/en/comps/Big5/defense/players/Big-5-European-Leagues-Stats#stats_defense'
+url_poss = 'https://fbref.com/en/comps/Big5/possession/players/Big-5-European-Leagues-Stats#stats_possession'
+
+# white Logo
+white_logo_url = \
+    "https://raw.githubusercontent.com/ricardoandreom/Liga-Portugal-2021-22/master/Half%20Space%20Branco.png"
+
+# black logo
+black_logo_url = "https://raw.githubusercontent.com/ricardoandreom/Liga-Portugal-2021-22/master/halfspace_analytics.png"
+
+# LANDING PAGE LINK
+SOCIAL_MEDIA = {
+    "Portfolio/Landing page": "https://ricardo-marques-digital-cv.streamlit.app/"
+}
+
+# set default colors
+text_color = 'white'
+background = 'black'
+
+font_normal = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/roboto/'
+                          'Roboto%5Bwdth,wght%5D.ttf')
+font_italic = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/roboto/'
+                          'Roboto-Italic%5Bwdth,wght%5D.ttf')
+font_bold = FontManager('https://raw.githubusercontent.com/google/fonts/main/apache/robotoslab/'
+                        'RobotoSlab%5Bwght%5D.ttf')
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+
+
+code = "\n".\
+    join([app.make_html(font) for font in sorted(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))])
+
+# creating the stats dfs
+df_standard = app.get_df(url_standard, ['Per 90 Minutes G-PK'])
+df_shotcreation = app.get_df(url_shotcreation, ['SCA SCA'])
+df_misc = app.get_df(url_misc, ['Aerial Duels Won%', 'Performance Fld', 'Performance Crs', 'Performance Recov'])
+df_shoot = app.get_df(url_shoot, ['Expected npxG', 'Expected npxG/Sh', 'Standard SoT'])
+df_passing = app.get_df(url_passing, ['Total Cmp', 'KP', 'xA', '1/3', 'Long Att', 'PrgP', 'Total Cmp%'])
+df_def = app.get_df(url_def, ['Tkl+Int', 'Blocks Sh', 'Clr'])
+df_poss = app.get_df(url_poss, ['Take-Ons Att', 'Take-Ons Succ%', 'Carries CPA', 'Receiving Rec', 'Receiving PrgR',
+                                'Carries PrgC'])
+
+# Define a list of dataframes
+dfs = [df_shotcreation, df_misc, df_shoot, df_passing, df_def, df_poss]
+
+# Loop through the list and drop the columns
+for df in dfs:
+    df.drop(columns=['Player', 'Squad', 'Age', 'Nation', 'Position', 'League', '90s', 'Position_2'], inplace=True)
+
+# concatenate all dat
+frames = [df_standard, df_shotcreation, df_misc, df_shoot, df_passing, df_def, df_poss]
+df = pd.concat(frames, axis=1, join='outer')
+
+########################################################################################################
+
+df = app.edit_df(df)
+
+# STREAMLIT APP
+
+# minutes filter
+# Getting the max value for the slider
+max_value = int(df['90s'].max() - 5)
+
+# APP TITLE
+st.title('Radar chart plotter ⚽📈')
+
+st.markdown("""
+    <p style='font-size: 20px; font-weight: bold; color: black;'> 🔴 Create percentile rank radar charts for any 
+    player in Europe's Top-5 Leagues</p>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <p style='font-size: 19px; font-weight: bold; color: black;'> 🔴 Compare a player stats with players who play in 
+    the same position</p>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <p style='font-size: 18px; font-weight: bold; color: black;'>Set the mininum 90's played to make the results
+     more representative of the reality.</p>
+""", unsafe_allow_html=True)
+
+st.write('#')
+# MAIN LOGO (WHITE)
+st.sidebar.image(white_logo_url, use_column_width=True)
+
+st.sidebar.markdown("***")
+
+st.sidebar.markdown("""
+    <p style='font-size: 26px; font-weight: bold; color: black;'>Radar chart inputs:</p>
+""", unsafe_allow_html=True)
+
+# Creating a slider that allows user to select an integer between 1 and max value minus 5
+nineties_played = st.sidebar.slider("**Select mininum 90s played:**", 1, max_value - 5, 1)
+
+player = st.sidebar.selectbox("**Name of the player:**", df['Player'])
+
+# GK note missing data
+st.sidebar.write('Note: No radar charts displayed for goalkeepers yet!')
+
+# portfolio url
+st.sidebar.write("#")
+cols = st.sidebar.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"**Made by @ricardoandreom 🚀⚽\n[{platform}]({link})**")
+
+#############################################################################################
+
+
+# filtering df by mininum 90s played
+df = df[df['90s'] >= nineties_played]
+
+# country filter
+country = 'POR'
+# df = df[df['Nation']=='country']
+
+# getting player main position
+player_position = df.set_index('Player').loc[player, 'Position']
+
+# filtering the df by player's position in order to make the comparison only this position players itself
+df = df[(df['Position'] == player_position)]
+
+
+df = app.percentiles_df(df)
+
+
+# getting the logo
+circle_logo = Image.open(urlopen(black_logo_url))
+
+# getting a list of player values
+player_values = list(df.set_index('Player').loc[player, app.templates_position_params(player_position)].values)
+
+app.show_radar_chart(player_values, df, player_position, player, nineties_played, circle_logo)
+
+
+# Save image button
+if st.download_button(label="Save image", data=app.save_image(), file_name=player + "_radar_chart.png", mime="image/png"):
+    st.write("Image saved successfully!")
+
+
+st.write('#')
+st.markdown("""
+    <p style='font-size: 20px; font-weight: bold; color: white;'>Data:</p>
+""", unsafe_allow_html=True)
+
+# plot dataframe
+all_cols = ['Nation', 'Squad', 'Age', '90s', 'Position', 'Position_2', 'League'] + \
+           app.templates_position_params(player_position)
+int_columns = ['Age'] + app.templates_position_params(player_position)
+df_plot_app = df.set_index('Player')[all_cols]
+df_plot_app['90s'] = df_plot_app['90s'].round(1)
+df_plot_app['League'] = df_plot_app['League'].fillna('Bundesliga')
+df_plot_app[int_columns] = df_plot_app[int_columns].astype(int)
+
+st.dataframe(df_plot_app)
